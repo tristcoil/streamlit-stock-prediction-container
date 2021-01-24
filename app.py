724da2b187dad1b05aed3d4e7d3291b0f2007d66
coil <michal.vasulka@gmail.com>
2021-01-24 13:41:40 +0100

    fixed workaround streamlit bug that doesnt display images sometimes

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 import time
-
+from PIL import Image
 
 st.write("""
-### stock price app
-stock closing price of given ticker
+### stock app
+predicts long entrypoints based on neural network classification
 """)
 
 
@@ -76,7 +76,7 @@ import matplotlib.pyplot as plt
 import datetime
 import time
 
-#newest yahoo API 
+#newest yahoo API
 import yfinance as yahoo_finance
 
 import talib as ta
@@ -85,9 +85,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-      
-def get_data(ticker):    
-    df = yf.download(tickers=ticker, start=start, end=end)    
+
+def get_data(ticker):
+    df = yf.download(tickers=ticker, start=start, end=end)
     df = df.reset_index()
     return df
 
@@ -105,7 +105,7 @@ def compute_technical_indicators(df):
     df['EMA70'] = ta.EMA(df['Adj Close'].values, timeperiod=70)
     df['EMA80'] = ta.EMA(df['Adj Close'].values, timeperiod=80)
     df['EMA90'] = ta.EMA(df['Adj Close'].values, timeperiod=90)
-    
+
     df['EMA100'] = ta.EMA(df['Adj Close'].values, timeperiod=100)
     df['EMA150'] = ta.EMA(df['Adj Close'].values, timeperiod=150)
     df['EMA200'] = ta.EMA(df['Adj Close'].values, timeperiod=200)
@@ -118,7 +118,7 @@ def compute_technical_indicators(df):
     df['RSI'] = ta.RSI(df['Adj Close'].values, timeperiod=14)
 
     df['normRSI'] = ta.RSI(df['Adj Close'].values, timeperiod=14) / 100.0
-    
+
     df.tail()
 
     return df
@@ -132,29 +132,29 @@ def compute_features(df):
     df['aboveEMA20'] = np.where(df['Adj Close'] > df['EMA20'], 1, 0)
     df['aboveEMA30'] = np.where(df['Adj Close'] > df['EMA30'], 1, 0)
     df['aboveEMA40'] = np.where(df['Adj Close'] > df['EMA40'], 1, 0)
-    
+
     df['aboveEMA50'] = np.where(df['Adj Close'] > df['EMA50'], 1, 0)
     df['aboveEMA60'] = np.where(df['Adj Close'] > df['EMA60'], 1, 0)
     df['aboveEMA70'] = np.where(df['Adj Close'] > df['EMA70'], 1, 0)
     df['aboveEMA80'] = np.where(df['Adj Close'] > df['EMA80'], 1, 0)
     df['aboveEMA90'] = np.where(df['Adj Close'] > df['EMA90'], 1, 0)
-    
+
     df['aboveEMA100'] = np.where(df['Adj Close'] > df['EMA100'], 1, 0)
     df['aboveEMA150'] = np.where(df['Adj Close'] > df['EMA150'], 1, 0)
     df['aboveEMA200'] = np.where(df['Adj Close'] > df['EMA200'], 1, 0)
 
     df['aboveUpperBB'] = np.where(df['Adj Close'] > df['upperBB'], 1, 0)
     df['belowLowerBB'] = np.where(df['Adj Close'] < df['lowerBB'], 1, 0)
-    
+
     df['aboveSAR'] = np.where(df['Adj Close'] > df['SAR'], 1, 0)
-   
+
     df['oversoldRSI'] = np.where(df['RSI'] < 30, 1, 0)
     df['overboughtRSI'] = np.where(df['RSI'] > 70, 1, 0)
 
 
     # very important - cleanup NaN values, otherwise prediction does not work
     df=df.fillna(0).copy()
-    
+
     df.tail()
 
     return df
@@ -171,18 +171,18 @@ def plot_train_data(df):
 
 
 def define_target_condition(df):
- 
+
     # price higher later - bad predictive results
-    #df['target_cls'] = np.where(df['Adj Close'].shift(-34) > df['Adj Close'], 1, 0)    
-    
+    #df['target_cls'] = np.where(df['Adj Close'].shift(-34) > df['Adj Close'], 1, 0)
+
     # price above trend multiple days later
     df['target_cls'] = np.where(df['Adj Close'].shift(-34) > df.EMA150.shift(-34), 1, 0)
 
     # important, remove NaN values
     df=df.fillna(0).copy()
-    
+
     df.tail()
-    
+
     return df
 
 
@@ -204,7 +204,7 @@ class Model(nn.Module):
         self.layer2 = nn.Linear(100, 30)
         self.layer3 = nn.Linear(30, 2)
         self.drop = nn.Dropout(0.2)
-        
+
     def forward(self, x):
         x = F.relu(self.layer1(x))
         x = self.drop(x)
@@ -233,31 +233,33 @@ class Model(nn.Module):
 def plot_stock_prediction(df, ticker):
     # plot  values and significant levels
     #fig = plt.figure(figsize=(30,7))
-    fig = plt.figure(figsize=(30,7))
-    ax = fig.add_subplot(1,2,1)
-    
+    fig = plt.figure(figsize=(10,7))
+    ####ax = fig.add_subplot(1,2,1)
+
     #ax.title('Predictive model ' + str(ticker))
-    ax.plot(df['Date'], df['Adj Close'], label='Adj Close', alpha=0.2)
+    plt.plot(df['Date'], df['Adj Close'], label='Adj Close', alpha=0.2)
 
-    ax.plot(df['Date'], df['EMA10'], label='EMA10', alpha=0.2)
-    ax.plot(df['Date'], df['EMA20'], label='EMA20', alpha=0.2)
-    ax.plot(df['Date'], df['EMA30'], label='EMA30', alpha=0.2)
-    ax.plot(df['Date'], df['EMA40'], label='EMA40', alpha=0.2)
-    ax.plot(df['Date'], df['EMA50'], label='EMA50', alpha=0.2)
-    ax.plot(df['Date'], df['EMA100'], label='EMA100', alpha=0.2)
-    ax.plot(df['Date'], df['EMA150'], label='EMA150', alpha=0.99)
-    ax.plot(df['Date'], df['EMA200'], label='EMA200', alpha=0.2)
+    plt.plot(df['Date'], df['EMA10'], label='EMA10', alpha=0.2)
+    plt.plot(df['Date'], df['EMA20'], label='EMA20', alpha=0.2)
+    plt.plot(df['Date'], df['EMA30'], label='EMA30', alpha=0.2)
+    plt.plot(df['Date'], df['EMA40'], label='EMA40', alpha=0.2)
+    plt.plot(df['Date'], df['EMA50'], label='EMA50', alpha=0.2)
+    plt.plot(df['Date'], df['EMA100'], label='EMA100', alpha=0.2)
+    plt.plot(df['Date'], df['EMA150'], label='EMA150', alpha=0.99)
+    plt.plot(df['Date'], df['EMA200'], label='EMA200', alpha=0.2)
 
 
-    ax.scatter(df['Date'], df['Buy']*df['Adj Close'], label='Buy', marker='^', color='magenta', alpha=0.15)
+    plt.scatter(df['Date'], df['Buy']*df['Adj Close'], label='Buy', marker='^', color='magenta', alpha=0.15)
     #ax.scatter(df.index, df['sell_sig'], label='Sell', marker='v')
+    plt.legend()
 
-    ax.legend()
+    plt.savefig('prediction.png')
 
-    st.write(fig)
+    #####st.write(fig)
+    image = Image.open('prediction.png')
+    st.image(image)
 
-
-    return None   
+    return None
 
 new_df = get_data(ticker)
 
@@ -272,34 +274,34 @@ saved_model = torch.load("iris-pytorch.pkl")
 
 
 def predict_timeseries(df):
-    
+
     # making sure we have good dimensions
     # column will be rewritten later
     df['Buy'] = df['target_cls']
-    
+
     for i in range(len(df)):
         X_cls_valid = [[df['aboveSAR'][i],df['aboveUpperBB'][i],df['belowLowerBB'][i],
                         df['normRSI'][i],df['oversoldRSI'][i],df['overboughtRSI'][i],
                         df['aboveEMA5'][i],df['aboveEMA10'][i],df['aboveEMA15'][i],df['aboveEMA20'][i],
                         df['aboveEMA30'][i],df['aboveEMA40'][i],df['aboveEMA50'][i],
                         df['aboveEMA60'][i],df['aboveEMA70'][i],df['aboveEMA80'][i],df['aboveEMA90'][i],
-                        df['aboveEMA100'][i]]]    
+                        df['aboveEMA100'][i]]]
 
-        x_test = Variable(torch.Tensor(X_cls_valid).float())    
-        
+        x_test = Variable(torch.Tensor(X_cls_valid).float())
+
         #####print('x_test',x_test)
-        
+
 
         #####print('i',i)
         prediction = np.argmax(saved_model(x_test[0]).detach().numpy(), axis=0)
-        #####print('prediction', prediction)        
-  
-    
+        #####print('prediction', prediction)
+
+
         df['Buy'][i] = prediction
 
 
-    print(df.head())    
-        
+    print(df.head())
+
     return df
 
 
@@ -326,32 +328,3 @@ plot_stock_prediction(temp_df, ticker)
 
 # ------------------ END OF                -----------------------
 # ------------------ MACHINE LEARNING PART -----------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
